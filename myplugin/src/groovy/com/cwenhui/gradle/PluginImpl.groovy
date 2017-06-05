47b6bd9ef0e53715c56b513a904d4a1ab0e48cd1
cwenhui <735506583@qq.com>
2017-06-05 14:21:09 +0800

    Initial commit

---
+++ myplugin/src/groovy/com/cwenhui/gradle/PluginImpl.groovy
@@ -0,0 +1,15 @@
+package com.cwenhui.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class PluginImpl implements Plugin<Project> {
+    /*void apply(Project project) {
+        project.gradle.addListener(new TimeListener())
+    }*/
+    void apply(Project project) {
+        project.task('testTask') << {
+            println "Hello gradle plugin"
+        }
+    }
+}
